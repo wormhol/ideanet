532a342ecb32b9d6741a0496b609781832f8acb1
wormhol <wormhol@gmail.com>
2015-09-25 04:59:27 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ideanet::Application.config.secret_key_base = '03d4981431cc137562f098c9a408abd8bd1eca873b0ee55a70570b79ec37b0e2aed9e79a89d183d74eddc96b86f38809e750551b904d0ec5ba86791bbcc07cea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ideanet::Application.config.session_store :cookie_store, key: '_ideanet_session'
